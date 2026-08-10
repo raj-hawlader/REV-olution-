c0a365e146b17af0682e64581fbee9f0ef7e24e0
Raj <148008370+raj-hawlader@users.noreply.github.com>
2026-08-10 23:08:06 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Hero from './Components/Hero/Hero';
 const App = () => {
   let heroData = [
     {text1: "Dive into",text2: "what you love"},
-    {text1: "Indulge",text: "your passions"},
+    {text1: "Indulge",text2: "your passions"},
     {text1: "Give in to",text2: "your passions"},
   ]
   const [heroCount, setheroCount] = useState(0);
